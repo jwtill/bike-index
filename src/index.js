0142cd738e44b82d8217b097c176aa4b2e869b76
Josh Tillinghast <jwtill@icloud.com>
2022-02-09 11:57:21 -0800

    Add HTML and build API call and basic parsing
    Co-authored-by: Jessica B <jessb-epicodus@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import BikeService from './js/bike-service.js';
 
-$(document).ready(function() {
-  console.log("Wassup");
+function getElements(response) {
+  if (response.bikes) {
+    $('#showTitle').text(`Description: ${response.bikes[0].title}`);
+  } else {
+    console.log(`${response}`);
+  }
+}
+
+async function makeApiCall(city) {
+  const response = await BikeService.getBikeIndex(city);
+  getElements(response);
+}
+
+$(document).ready(function () {
+  $("form#user-input").submit(function () {
+    event.preventDefault();
+    let city = $("#city").val();
+    makeApiCall(city);
+  });
 });
-
