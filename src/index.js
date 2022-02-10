9a844411533eda58c203d9aadbdff41b68171126
Josh Tillinghast <jwtill@icloud.com>
2022-02-10 12:01:11 -0800

    WIP lunch
    Co-authored-by: Jessica B <jessb-epicodus@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -4,33 +4,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import BikeService from './js/bike-service.js';
 
-function getElements(response) {
-  if (response.bikes[0]) {
-    for (let i = 0; i < response.bikes.length; i++) {
-      $('#showTitle').append(`Description: ${response.bikes[i].title} <br>`);
-    }
-  }
-  else {
-    $('#showError').text(`There was an error ${response}`);
-  }
+function clearFields() {
+  $("#city").val("");
+  $("#showResults").text("");
+  $("#showError").text("");
+  $("#noResults").text("")
 }
 
-// function parseDate(milSecDate) {
-//   return milSecDate.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}); 
-// }
+function getElements(response) {
+  let today = new Date;
+  let lastWeek = new Date(today.setDate(today.getDate() - 7));
+  if (response.bikes[0]) {
+    for (let i = 0; i < response.bikes.length; i++) {
+      const stolenDate = new Date((response.bikes[i].date_stolen) * 1000);
+      if (stolenDate >= lastWeek && (response.bikes[i].status === "stolen")) {
+        $('#showResults').append(`Description: ${response.bikes[i].title} <br>  Date Stolen: ${stolenDate}<br> Stolen From: ${response.bikes[i].stolen_location} <br><br><br>`);
+      } else {
+        $("#noResults").text("No bikes reported stolen in the past week.");   //could add button to expand date search
+      }
+    }
+  } else {
+    $('#showError').text(`There was an error: ${response}`);
+  }
+}
 
 async function makeApiCall(city) {
   const response = await BikeService.getBikeIndex(city);
   getElements(response);
 }
 
-$(document).ready(function () {
-  $("form#user-input").submit(function () {
+$(document).ready(function() {
+  $("form#user-input").submit(function() {
     event.preventDefault();
     let city = $("#city").val();
-    makeApiCall(city);
+    if (city === "") {
+      clearFields();
+      $("#showError").text("Please enter a valid city.");
+    }
+    else {
+      clearFields();
+      makeApiCall(city);
+    }
   });
 });
-
-
-// response.bikes[0].title
--- src/js/bike-service.js
+++ src/js/bike-service.js
@@ -1,12 +1,13 @@
 export default class BikeService {
   static async getBikeIndex (city) {
     try {
-      const response = await fetch (`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${city}&distance=10&stolenness=stolen`);
+      const response = await fetch(`https://bikeindex.org:443/api/v3/search?page=1&per_page=100&location=${city}&distance=10&stolenness=proximity`);
       if (!response.ok) {
-        throw Error (response.statusText);
+        throw Error(response.statusText);
       }
       return response.json();
-    } catch (error) {
+    } catch(error) {
+      console.log(error.message);
       return error.message;
     }
   }
